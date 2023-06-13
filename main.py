13a5de112f94c3a8de394f6e9b17999ef16c0bb1
spiddacino <chidi.okonta@gmail.com>
2023-06-13 21:23:55 +0100

    Final Addition

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import tasks
 
 app = FastAPI()
 
+with open("languages.txt") as f:
+    languages = f.read().split(", ")
+
+class Translation(BaseModel):
+    text: str
+    base_lang: str
+    final_lang: str
+
+    @validator('base_lang', 'final_lang')
+    def valid_lang(cls, v):
+        if v not in languages:
+            raise ValueError('Language not supported')
+        return v
+
 ## Route 1: /
 ## Test if everything is working
 ##{ "message": "Hello World!"}
@@ -14,7 +28,18 @@ def get_root():
 ## Route 2: /translate
 ## Take a translation request and store it in the database
 ## Return the translation id
+@app.post("/translate")
+def post_translation(t: Translation, background_tasks: BackgroundTasks):
+    #Store the translation in the database
+    t_id = tasks.store_translation(t)
+    #run translation in the background
+    background_tasks.add_task(tasks.run_translation, t.id)
+    return {"task_id": t_id}
+    
 
 ## Route 3: /result
 ## Take a translation id
 ## return the result if it is ready
+@app.get("/result")
+def get_result(t_id: int):
+    return {"translation": tasks.find_translation(t_id)}
--- tasks.py
+++ tasks.py
@@ -1,8 +1,37 @@
+from models import Translation
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+#t5-base t5-large
+tokenizer = T5Tokenizer.from_pretrained('t5-small', model_max_length=512)
+translator = T5ForConditionalGeneration.from_pretrained('t5-small')
+
 ## store translation
 ## Translation request and save to database
+def store_translation(t):
+    model = TranslationModel(text=t.text, base_lang=t.base_lang, final_lang=t.final_lang)
+    model.save()
+    return model.id
 
 ## run translation
 ## Run a pretrained deep learning model to translate the text
+def run_translation(t_id: int):
+    model = TranslationModel.get_by_id(t_id)
 
-## find_translation
+    prefix = f"translate {model.text} from {model.base_lang} to {model.final_lang}: "
+    input_ids = tokenizer(prefix, return_tensors="pt").input_ids
+
+    outputs = translator.generate(input_ids, max_new_tokens=512)
+    translation = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    model.translation = translation
+    model.save()
+
+
+
+## find_translation 
 ## retrieve  translation from database
+def find_translation(t_id: int):
+    model = TranslationModel.get_by_id(t_id)
+    Translation = model.translation
+    if translation is None:
+        translation = "Translation in progress"
+    return translation
